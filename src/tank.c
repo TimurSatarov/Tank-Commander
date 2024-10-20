d39373288638bd08b19eb521b0c5d3ce4f0c65a3
TimurSatarov <timursatarov3@gmail.com>
2024-10-20 13:38:44 +0200

    modify rotation of turret

--- src/tank.c
+++ src/tank.c
@@ -5,39 +5,64 @@ void initTank(Tank* tank, float x, float y, SDL_Texture* bodyTexture, SDL_Textur
     tank->x = x;
     tank->y = y;
     tank->rotation = 0.0f; 
-    
+    tank->headRotation = 0.0f;
     tank->bodyTexture = bodyTexture;
     tank->headTexture = headTexture;
 }
 
 void moveTank(Tank* tank, const Uint8* keyState) {
-    const float speed = 5.0f; 
+    const float speed = 2.5f; 
+    const float backSpeed = 1.0f;
 
     if (keyState[SDL_SCANCODE_UP]) {
         tank->x += speed * cos(tank->rotation * M_PI / 180.0f);
         tank->y += speed * sin(tank->rotation * M_PI / 180.0f);
     }
     if(keyState[SDL_SCANCODE_DOWN]) {
-        tank->x -= speed * cos(tank->rotation * M_PI / 180.0f);
-        tank->y -= speed * sin(tank->rotation * M_PI / 180.0f);
+        tank->x -= backSpeed * cos(tank->rotation * M_PI / 180.0f);
+        tank->y -= backSpeed * sin(tank->rotation * M_PI / 180.0f);
     }
     if(keyState[SDL_SCANCODE_LEFT]) {
-        tank->rotation -= 5.0f;
+        tank->rotation -= 2.0f;
     }
     if(keyState[SDL_SCANCODE_RIGHT]) {
-        tank->rotation += 5.0f;
+        tank->rotation += 2.0f;
     }
     if(keyState[SDL_SCANCODE_A]){
-
+        tank->headRotation -= 3.0f;
+    }
+    if(keyState[SDL_SCANCODE_D]){
+        tank->headRotation += 3.0f;
     }
 }
 
 void renderTank(SDL_Renderer* renderer, Tank* tank) {
     // Render body
-    SDL_Rect bodyRect = { (int)tank->x, (int)tank->y, 40, 50 };
+    SDL_Rect bodyRect = { (int)tank->x, (int)tank->y, 36, 50 };
     SDL_RenderCopyEx(renderer, tank->bodyTexture, NULL, &bodyRect, tank->rotation + 90, NULL, SDL_FLIP_NONE);
 
+    int headWidth = 30; // Width of the head image
+    int headHeight = 50;
+
+    
     // Render head
-    SDL_Rect headRect = { (int)tank->x + 15, (int)tank->y + 15, 20, 20 }; 
-    SDL_RenderCopyEx(renderer, tank->headTexture, NULL, &headRect, tank->rotation, NULL, SDL_FLIP_NONE);
+    //SDL_Rect headRect = { (int)tank->x -2, (int)tank->y - 10, headWidth, headHeight }; 
+    float offsetX = -2.5f;  // X offset from the center of the body
+    float offsetY = 0.0f;  // Y offset from the center of the body
+
+
+    float rotatedOffsetX = offsetX * cos(tank->rotation * M_PI / 180.0f) - offsetY * sin(tank->rotation * M_PI / 180.0f);
+    float rotatedOffsetY = offsetX * sin(tank->rotation * M_PI / 180.0f) + offsetY * cos(tank->rotation * M_PI / 180.0f);
+
+    SDL_Rect headRect = {
+        (int)(tank->x + (rotatedOffsetX + 4)),
+        (int)(tank->y + (rotatedOffsetY -10)),
+        headWidth,
+        headHeight
+    };
+    // Correct rotation point of head
+    SDL_Point headCenter = {(headWidth / 2), (headHeight / 2) + 10};
+
+
+    SDL_RenderCopyEx(renderer, tank->headTexture, NULL, &headRect, tank->headRotation, &headCenter, SDL_FLIP_NONE);
 }

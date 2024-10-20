263ca01259b0535f8d7633b3bce62e2bdd906409
TimurSatarov <timursatarov3@gmail.com>
2024-10-20 16:44:08 +0200

    logic of fire function

--- src/tank.c
+++ src/tank.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdbool.h>
 #include "tank.h"
 
 void initTank(Tank* tank, float x, float y, SDL_Texture* bodyTexture, SDL_Texture* headTexture) {
@@ -47,6 +48,7 @@ void renderTank(SDL_Renderer* renderer, Tank* tank) {
     
     // Render head
     //SDL_Rect headRect = { (int)tank->x -2, (int)tank->y - 10, headWidth, headHeight }; 
+
     float offsetX = -2.5f;  // X offset from the center of the body
     float offsetY = 0.0f;  // Y offset from the center of the body
 
@@ -64,5 +66,45 @@ void renderTank(SDL_Renderer* renderer, Tank* tank) {
     SDL_Point headCenter = {(headWidth / 2), (headHeight / 2) + 10};
 
 
-    SDL_RenderCopyEx(renderer, tank->headTexture, NULL, &headRect, tank->headRotation, &headCenter, SDL_FLIP_NONE);
+    SDL_RenderCopyEx(renderer, tank->headTexture, NULL, &headRect, tank->headRotation + 90, &headCenter, SDL_FLIP_NONE);
+}
+
+
+void fireBullet(Tank* tank, Bullet bullets[],SDL_Texture* bulletTexture){
+    for(int i = 0;i < MAX_BULLETS; i++){
+        if(!bullets[i].active){
+            bullets[i].x = tank->x + 15;
+            bullets[i].y = tank->y + 22;
+            bullets[i].dx = 10.0f * cos(tank -> headRotation * M_PI / 180.0f);
+            bullets[i].dy = 10.0f * sin(tank -> headRotation * M_PI / 180.0f);
+            bullets[i].active = true;
+            bullets[i].bulletTexture = bulletTexture;
+            break;
+        }
+    }
+}
+
+
+void moveBullets(Bullet bullets[]){
+    for(int i = 0; i < MAX_BULLETS; i++){
+        if(bullets[i].active){
+            bullets[i].x += bullets[i].dx;
+            bullets[i].y += bullets[i].dy;
+
+            if(bullets[i].x < 0 || bullets[i].x > 800 || bullets[i].y < 0 || bullets[i].y > 600){
+                bullets[i].active = false;
+            }
+        }
+    }
+}
+
+
+void renderBullets(SDL_Renderer* renderer, Bullet bullets[]){
+
+    for(int i = 0;i < MAX_BULLETS; i++){
+        if(bullets[i].active){
+            SDL_Rect bulletRect = {(int)bullets[i].x,(int)bullets[i].y, 10, 10};
+            SDL_RenderCopy(renderer, bullets[i].bulletTexture, NULL, &bulletRect);
+        }
+    }
 }

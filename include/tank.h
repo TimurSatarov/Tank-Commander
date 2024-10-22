fd5a78a732e8e043157b90e638cc0141179d8647
TimurSatarov <timursatarov3@gmail.com>
2024-10-23 00:22:00 +0200

    add more features

--- include/tank.h
+++ include/tank.h
@@ -14,6 +14,19 @@ typedef struct {
     SDL_Texture* headTexture;   
 } Tank;
 
+typedef struct{
+    Tank tank;
+    bool active;
+} EnemyTank;
+
+
+typedef struct{
+    SDL_Rect rect;
+    SDL_Texture* texture;
+} Obstacle;
+
+
+
 
 typedef struct{
     float x, y;
@@ -28,7 +41,14 @@ void renderTank(SDL_Renderer* renderer, Tank* tank);
 
 //bullet func
 void fireBullet(Tank* tank, Bullet bullet[], SDL_Texture* bulletTexture);
-void moveBullets(Bullet bullets[]);
+void moveBullets(Bullet bullets[], Obstacle obstacle[], int numObstacles);
 void renderBullets(SDL_Renderer* renderer, Bullet bullet[]);
+
+bool checkCollision(float bulletX, float bulletY, SDL_Rect wall); 
+void initEnemyTank(EnemyTank *enemy, float x, float y, SDL_Texture* bodyTexture, SDL_Texture* headTexture);
+void updateEnemy(EnemyTank* enemy, Tank* playerTank);
+void renderEnemyTank(SDL_Renderer* renderer, EnemyTank* enemy);
+void checkBulletEnemyCollision(Bullet bullets[], EnemyTank enemies[], int numEnemies, int* score);
+void renderEnemyTank(SDL_Renderer* renderer, EnemyTank* enemy);
 
 #endif

5e8d1df4cffb316daf24292a22e33bb2ac252a52
TimurSatarov <timursatarov3@gmail.com>
2024-10-23 00:57:36 +0200

    change

--- src/tank.c
+++ src/tank.c
@@ -1,6 +1,14 @@
 #include <math.h>
 #include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <stdlib.h>
 #include "tank.h"
+
+
+#define TANK_BODY_WIDTH 36
+#define TANK_BODY_HEIGHT 50
+#define TANK_HEAD_WIDTH 30
+#define TANK_HEAD_HEIGHT 50
 
 void initTank(Tank* tank, float x, float y, SDL_Texture* bodyTexture, SDL_Texture* headTexture) {
     tank->x = x;
@@ -35,17 +43,15 @@ void moveTank(Tank* tank, const Uint8* keyState) {
     if(keyState[SDL_SCANCODE_D]){
         tank->headRotation += 3.0f;
     }
+    
 }
+
 
 void renderTank(SDL_Renderer* renderer, Tank* tank) {
     // Render body
-    SDL_Rect bodyRect = { (int)tank->x, (int)tank->y, 36, 50 };
+    SDL_Rect bodyRect = { (int)tank->x, (int)tank->y, TANK_BODY_WIDTH, TANK_BODY_HEIGHT };
     SDL_RenderCopyEx(renderer, tank->bodyTexture, NULL, &bodyRect, tank->rotation + 90, NULL, SDL_FLIP_NONE);
 
-    int headWidth = 30; // Width of the head image
-    int headHeight = 50;
-
-    
     // Render head
     //SDL_Rect headRect = { (int)tank->x -2, (int)tank->y - 10, headWidth, headHeight }; 
 
@@ -59,11 +65,11 @@ void renderTank(SDL_Renderer* renderer, Tank* tank) {
     SDL_Rect headRect = {
         (int)(tank->x + (rotatedOffsetX + 4)),
         (int)(tank->y + (rotatedOffsetY -10)),
-        headWidth,
-        headHeight
+        TANK_HEAD_WIDTH,
+        TANK_HEAD_HEIGHT
     };
     // Correct rotation point of head
-    SDL_Point headCenter = {(headWidth / 2), (headHeight / 2) + 10};
+    SDL_Point headCenter = {(TANK_HEAD_WIDTH / 2), (TANK_HEAD_HEIGHT / 2) + 10};
 
 
     SDL_RenderCopyEx(renderer, tank->headTexture, NULL, &headRect, tank->headRotation + 90, &headCenter, SDL_FLIP_NONE);
@@ -85,7 +91,7 @@ void fireBullet(Tank* tank, Bullet bullets[],SDL_Texture* bulletTexture){
 }
 
 
-void moveBullets(Bullet bullets[]){
+void moveBullets(Bullet bullets[], Obstacle obstacle[], int numObstacles){
     for(int i = 0; i < MAX_BULLETS; i++){
         if(bullets[i].active){
             bullets[i].x += bullets[i].dx;
@@ -94,10 +100,22 @@ void moveBullets(Bullet bullets[]){
             if(bullets[i].x < 0 || bullets[i].x > 800 || bullets[i].y < 0 || bullets[i].y > 600){
                 bullets[i].active = false;
             }
+
+            for(int j = 0;j < numObstacles; j++){
+                if(checkCollision(bullets[i].x, bullets[i].y, obstacle[j].rect)){
+                    bullets[i].active = false;
+                    break;
+                }
+            }
         }
     }
 }
 
+
+bool checkCollision(float bulletX, float bulletY, SDL_Rect wall) {
+    return (bulletX >= wall.x && bulletX <= (wall.x + wall.w) &&
+            bulletY >= wall.y && bulletY <= (wall.y + wall.h));
+}
 
 void renderBullets(SDL_Renderer* renderer, Bullet bullets[]){
 
@@ -107,4 +125,58 @@ void renderBullets(SDL_Renderer* renderer, Bullet bullets[]){
             SDL_RenderCopy(renderer, bullets[i].bulletTexture, NULL, &bulletRect);
         }
     }
+}
+
+int score = 0;  // score
+
+void checkBulletEnemyCollision(Bullet bullets[], EnemyTank enemies[], int numEnemies, int* score) {
+    for (int i = 0; i < MAX_BULLETS; i++) {
+        if (bullets[i].active) {
+            for (int j = 0; j < numEnemies; j++) {
+                if (enemies[j].active) {
+                    SDL_Rect enemyRect = { (int)enemies[j].tank.x, (int)enemies[j].tank.y, 50, 50 };
+                    if (checkCollision(bullets[i].x, bullets[i].y, enemyRect)) {
+                        bullets[i].active = false;
+                        enemies[j].active = false;
+                        (*score)++;  // Increase score
+                        printf("Enemy hit! Score: %d\n", *score);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+void initEnemyTank(EnemyTank *enemy, float x, float y, SDL_Texture* bodyTexture, SDL_Texture* headTexture){
+    initTank(&enemy->tank, x, y, bodyTexture, headTexture);
+    enemy->active = true;
+}
+
+
+void updateEnemy(EnemyTank* enemy, Tank* playerTank){
+    if(!enemy->active) return;
+
+    float dx = playerTank->x - enemy->tank.x;
+    float dy = playerTank->y - enemy->tank.y;
+
+    float length = sqrt(dx * dx + dy * dy);
+    dx /= length;
+    dy /= length;
+
+    enemy->tank.x += dx * 2.0f;
+    enemy->tank.y += dy * 2.0f;
+
+    enemy->tank.rotation = atan2(dy, dx) * 180.0f/M_PI;
+
+    enemy->tank.headRotation = enemy->tank.rotation;
+}
+void renderEnemyTank(SDL_Renderer* renderer, EnemyTank* enemy){
+    if(enemy->active){
+        renderTank(renderer, &enemy->tank);
+    }
+
 }

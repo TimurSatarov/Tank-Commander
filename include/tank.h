e6cc9d2939b00e6108e1788061e476e6b2d90eb8
TimurSatarov <timursatarov3@gmail.com>
2024-10-31 21:51:47 +0100

    rebuild the structure

--- include/tank.h
+++ include/tank.h
@@ -7,49 +7,51 @@
 #define MAX_BULLETS 1
 
 typedef struct {
-    float x, y;        // tank position
-    float rotation;    
-    float headRotation;
+    float x, y;                 
+    float rotation;             
+    float headRotation;         
     SDL_Texture* bodyTexture;   
     SDL_Texture* headTexture;   
 } Tank;
 
-typedef struct{
-    Tank tank;
-    bool active;
+typedef struct {
+    Tank tank;           
+    float rotation;      
+    bool active;         
+    float health;        
 } EnemyTank;
 
-
-typedef struct{
-    SDL_Rect rect;
-    SDL_Texture* texture;
+typedef struct {
+    SDL_Rect rect;       
+    SDL_Texture* texture; 
 } Obstacle;
 
-
-
-
-typedef struct{
-    float x, y;
-    float dx, dy;
+typedef struct {
+    float x, y;          
+    float dx, dy;        
+    float direction; 
     bool active;
-    SDL_Texture* bulletTexture;
-}Bullet;
+    bool firedByEnemy;
+    SDL_Texture* bulletTexture; 
+} Bullet;
 
 void initTank(Tank* tank, float x, float y, SDL_Texture* bodyTexture, SDL_Texture* headTexture);
 void moveTank(Tank* tank, const Uint8* keyState, Obstacle obstacles[], int numObstacles, EnemyTank enemies[], int numEnemies);
 void renderTank(SDL_Renderer* renderer, Tank* tank);
 
-//bullet func
-void fireBullet(Tank* tank, Bullet bullet[], SDL_Texture* bulletTexture);
-void moveBullets(Bullet bullets[], Obstacle obstacle[], int numObstacles);
-void renderBullets(SDL_Renderer* renderer, Bullet bullet[]);
+void fireBullet(Tank* tank, Bullet bullets[], SDL_Texture* bulletTexture);
+void moveBullets(Bullet bullets[], Obstacle obstacles[], int numObstacles);
+void renderBullets(SDL_Renderer* renderer, Bullet bullets[]);
 
 bool checkCollision(float bulletX, float bulletY, SDL_Rect wall); 
 bool checkRectCollision(SDL_Rect a, SDL_Rect b);
+
 void initEnemyTank(EnemyTank *enemy, float x, float y, SDL_Texture* bodyTexture, SDL_Texture* headTexture);
 void updateEnemy(EnemyTank* enemy, Tank* playerTank);
+void moveEnemies(EnemyTank enemies[], int numEnemies, Tank* playerTank);
 void renderEnemyTank(SDL_Renderer* renderer, EnemyTank* enemy);
-void checkBulletEnemyCollision(Bullet bullets[], EnemyTank enemies[], int numEnemies, int* score);
-void renderEnemyTank(SDL_Renderer* renderer, EnemyTank* enemy);
+void enemyFire(EnemyTank* enemy, Bullet bullets[], int numBullets, Tank* playerTank, SDL_Texture* bulletTexture);
+
+void checkBulletEnemyCollision(Bullet bullets[], EnemyTank enemies[], int numEnemies, Tank* tank, int* score);
 
 #endif
